3ebeb1e69269232d3912c0b490a5fbdf518164c6
GregorVolcanjk <65243657+GregorVolcanjk@users.noreply.github.com>
2021-01-02 12:23:35 +0100

    Added splash screen

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -16,6 +16,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -48,6 +49,10 @@ public class MainActivity extends AppCompatActivity implements CheckboxRecyclerA
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
         mRecyclerView= findViewById(R.id.recyclerview);
         findViewById(R.id.fab).setOnClickListener(this);
         mRepository=new Repository(this);
